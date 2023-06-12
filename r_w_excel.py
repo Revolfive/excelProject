4527abc1457d64565df98c5275773487b441bfc9
Revolfive <1440664241@qq.com>
2023-06-12 16:43:40 +0800

    feat:设置图片单元格偏移按文件所占内存大小存储文件

--- r_w_excel.py
+++ r_w_excel.py
@@ -6,6 +6,9 @@
 import requests
 from openpyxl import Workbook, load_workbook
 from openpyxl.drawing.image import Image
+from openpyxl.drawing.spreadsheet_drawing import AnchorMarker, OneCellAnchor
+from openpyxl.drawing.xdr import XDRPositiveSize2D
+from openpyxl.utils.units import pixels_to_EMU
 import threading
 import os
 import asyncio
@@ -13,6 +16,7 @@ import aiohttp
 
 lock = threading.Lock()
 count = 0
+max_size = 15  # 单文件最大占用内存
 
 
 def transpose(matrix):
@@ -79,6 +83,17 @@ def read_excel():
     return excel_data
 
 
+def offset_img(img, col, row):
+    """精确设置图片位置，偏移量以万为单位进行微调吧，具体计算公式太麻烦了
+    row column 的索引都是从0开始的，我这里要把图片插入到单元格B10
+    """
+    p2e = pixels_to_EMU
+    h, w = img.height, img.width
+    size = XDRPositiveSize2D(p2e(w), p2e(h))
+    marker = AnchorMarker(col=col, colOff=60000, row=row, rowOff=60000)
+    img.anchor = OneCellAnchor(_from=marker, ext=size)
+
+
 def write_excel(data, i, distance):
     # 创建Workbook对象
     workbook = Workbook()
@@ -91,7 +106,10 @@ def write_excel(data, i, distance):
         worksheet.append(template)
 
     # 将嵌套列表写入工作表
-    begin, end = (i * distance, (i + 1) * distance)
+    if type(distance) is list:
+        begin, end = distance
+    else:
+        begin, end = (i * distance, (i + 1) * distance)
     for index, row in enumerate(data[begin:end]):
         url = row[0]
 
@@ -104,11 +122,13 @@ def write_excel(data, i, distance):
             _filename = '%s.%s' % (row[2], url.split('/')[-1].split('.')[-1])
             # 获取图片并调整图片大小和单元格大小
             img = Image(rf'img\{_filename}')
-            img.width, img.height = (img.width / 4, img.height / 4)
-            worksheet.column_dimensions['A'].width = img.width / 5
+            img.width, img.height = (img.width * 72 / img.height, 72)
+            offset_img(img, col=0, row=index + 2)
 
-            worksheet.row_dimensions[index + 3].height = img.height
-            worksheet.add_image(img, f'A{index + 3}')  # 插入图片
+            worksheet.column_dimensions['A'].width = 10 + 2
+
+            worksheet.row_dimensions[index + 3].height = 60 + 12
+            worksheet.add_image(img)  # 插入图片
         except FileNotFoundError:
             row[0] = url
             print(f'找不到图片信息，表格内容:{row}')
@@ -122,6 +142,32 @@ def write_excel(data, i, distance):
     print(f'当前已完成excel进度：{count}')
     lock.release()
     workbook.save(filename=f'newdata\example{begin}-{end}.xlsx')
+
+
+def write_excel_for_size(data):
+    current_size = 0
+    b_to_e = [0, 0]
+    b_to_e_lists = []
+    for index, row in enumerate(data):
+        if b_to_e[1] > 0:
+            b_to_e = [index - 1, 0]
+        url = row[0]
+        try:
+            _filename = '%s.%s' % (row[2], url.split('/')[-1].split('.')[-1])
+            # 获取图片大小
+            size = os.path.getsize(rf'img\{_filename}') / 1024 / 1024
+            current_size += size
+            print(current_size)
+            if current_size > max_size:
+                current_size = 0
+                b_to_e[1] = index
+                b_to_e_lists.append(b_to_e)
+        except:
+            pass
+    b_to_e[1] = len(data)
+    b_to_e_lists.append(b_to_e)
+    for b2e in b_to_e_lists:
+        write_excel(data=data, i=0, distance=b2e)
 
 
 def get_pic(data, i, distance):
@@ -145,35 +191,6 @@ def get_pic(data, i, distance):
         lock.release()
 
 
-async def async_download(data, i, distance):
-    # 发送请求
-    # 得到图片内容
-    # 保存到文件
-    begin, end = (i * distance, (i + 1) * distance)
-    for row in data[begin:end]:
-        url = row[0]
-        try:
-            _filename = url.split('/')[-1]
-            _filename = '%s.%s' % (row[2], url.split('/')[-1].split('.')[-1])
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as resp:
-                    # 将得到的请求保存到文件中
-                    with open(fr'img\{_filename}', 'wb') as f:
-                        f.write(await resp.content.read())
-                        global count
-                        count += 1
-                        print(f'当前下载图片进度：{count}/{len(data)}')
-        except:
-            print(f'找不到图片信息，表格内容:{row}')
-
-    print("下载完成")
-
-
-async def main(fun, data, distance):
-    tasks = [asyncio.create_task(async_download(data, i, distance)) for i, j in enumerate(data[::distance])]
-    await asyncio.wait(tasks)
-
-
 def run(fun, data, distance):
     th_list = []
     for i, j in enumerate(data[::distance]):
@@ -186,24 +203,7 @@ def run(fun, data, distance):
     for th_join in th_list:
         th_join.join()
 
-# def a():
-#     from openpyxl import Workbook, load_workbook
-#     from openpyxl.drawing.image import Image
-#     from PIL import Image as PILImage
-#
-#     wb = load_workbook('example.xlsx')
-#     ws = wb.active
-#
-#     img = PILImage.open('GX0007.png')
-#     img_width, img_height = img.size
-#     img = Image('GX0007.png')
-#     ws.add_image(img, 'B2')
-#
-#     cell = ws['B2']
-#     cell_width = ws.column_dimensions['A'].width
-#     cell_height = ws.row_dimensions[1].height
-#     print(cell_width, cell_height)
-# a()
+
 if __name__ == '__main__':
     print('-----------只读取获取到的第一个excel文件-----------')
     print('-----------开始读取excel数据-----------')
@@ -214,7 +214,8 @@ if __name__ == '__main__':
         print('请输入:')
         print('1:将图片下载到本地')
         print('2:生成新的款型库导入模板')
-        print('3:退出')
+        print(f'3:按当文件大小不超过{max_size}M写入excel')
+        print('4:退出')
 
         model = int(input('>>'))
         if model == 1:
@@ -237,9 +238,9 @@ if __name__ == '__main__':
                 else:
                     print('输入不合法，请重新输入')
         elif model == 3:
+            write_excel_for_size(_data)
+            break
+        elif model == 4:
             break
         else:
             print('输入不合法，请重新输入')
-
-    # run(fun=get_pic, data=_data, distance=1000)  # 获取图片  distance为单个线程下载图片的数量
-    # run(fun=write_excel, data=_data, distance=100)  # 写入excel  distance单个文件包含的款式数量
